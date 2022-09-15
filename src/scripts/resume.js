65f088aeddad98ac56005b18b42f78c025df6acd
Vladimir Andrushchenko <andrushchenko.vladimir@gmail.com>
2022-09-15 17:58:59 +0300

    started turning page into template

--- src/scripts/resume.js
+++ src/scripts/resume.js
@@ -1,5 +1,5 @@
 const resumeSelectorsConfig = {
-    template: '#resume',
+    resumeTemplate: '#resume',
     resume: '.resume',
     salary: '.resume__salary',
     title: '.resume__title',
@@ -9,11 +9,22 @@ const resumeSelectorsConfig = {
     schedules: '.resume__schedules',
     additionalInfoItems: '.resume__additional-info-list',
     workExperiencesList: '.resume__work-experience-list',
+    workExperienceItemTemplate: '#resume__work-experience-item',
+    workExperienceItem: '.resume__work-experience-item',
+    prevJobName: '.resume__prev-job-name',
+    prevJobTime: '.resume__prev-job-time',
+    prevJobDescription: '.resume__prev-job-description',
+    weakSidesText: '.resume__cons-text',
+    strongSidesText: '.resume__pros-text',
+    skillsList: '.resume__skills-list',
+    motivationsList: '.resume__motivation-list',
+    hobbies: '.resume__hobbies-text'
 };
 
 const resumeClassesConfig = {
     scheduleItem: 'resume__schedule',
-    additionalInfoItemsElement: 'resume__additional-info-element'
+    additionalInfoItemsElement: 'resume__additional-info-element',
+    skillsItem: 'resume__skill'
 };
 
 function fetchResumeData() {
@@ -29,7 +40,16 @@ function fetchResumeData() {
 }
 
 function makeResumeSection(data) {
-    const resumeSectionNode = document.querySelector('#resume').textContent.querySelector('.resume').cloneNode(true);
+    const resumeSectionNode = document.querySelector('#resume').content.querySelector('.resume').cloneNode(true);
 
-    const { name, birthday, about, contacts, job, info } = data;
+    // const { name, birthday, about, contacts, job, info } = data;
+
+    return resumeSectionNode;
 }
+
+function addResumeToPage() {
+    const main = document.querySelector('.main');
+    main.append(makeResumeSection());
+}
+
+addResumeToPage();
