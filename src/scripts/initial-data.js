779ba055242b870cf8b1e9fd1fabc1df6d80e14c
BadStandUp <semen.kalashnikov@mail.ru>
2022-09-15 23:42:51 +0300

    feat: initial data

---
+++ src/scripts/initial-data.js
@@ -0,0 +1,14 @@
+const actualVacanciesInitialCards = [
+  {
+    subtitle: 'PR менеджер',
+    description: 'Публиковать новости и жизнь Фонда в социальные сети, отвечать на комментарии и быть в курсе всех дел',
+    city: 'Новороссийск',
+    job: '',
+    salary: '35000',
+    schedule: '',
+    workExperience: '',
+    logo: '',
+    hoverTitle: '"Антон тут рядом"',
+    hoverAddress: 'г.Новороссийск, ул.Видова, д.214',
+  }
+]
