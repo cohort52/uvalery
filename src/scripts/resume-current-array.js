3d02127cec9179dee11bf59eae8a1d023c1d5e4f
Anastacia-Tesli <teslina.anastacia@yandex.ru>
2022-09-16 13:35:08 +0300

    improved

---
+++ src/scripts/resume-current-array.js
@@ -0,0 +1,34 @@
+// Массив карточек-резюме
+
+const resumeList = [
+  {
+    image: 'https://images.unsplash.com/photo-1610655507808-a59293f4e332?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8d29tYW4lMjBwb3J0cmFpdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+    job: 'Психолог, координатор',
+    name: 'Иванова Мария Ивановна',
+    description: 'Творчески развиваюсь постоянно, изучаю новые методы и т.д. Ищу свою команду для активной деятельности во благо.',
+    skills: ['SMM', 'Instgram', 'Администратор', 'Открытость'],
+    grafic: 'Удаленно 5/2 2/2',
+    pay: '35 000 руб',
+    published: '1 день назад'
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+    job: 'Преподватель, менеджер',
+    name: 'Смирнова Дарья Петровна',
+    description: 'Здравствуйте, меня зовут Дарья. Я занимаюсь преподаванием более 5 лет. Имею опыт работы с детьми от 4 и до 18 лет. Также занимаюсь со студентами и взрослыми. Преподавала в unknown школе. Нахожу ко всем индивидуальный подход.',
+    skills: ['Креативность', 'коммуникабельность'],
+    grafic: 'В офисе 5/2',
+    pay: '40 000 руб',
+    published: '4 дня назад'
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1560298803-1d998f6b5249?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    job: 'Прогаммист, системный администратор',
+    name: 'Иванов Иван Иванович',
+    description: 'Здравствуйте, меня зовут Иван. ',
+    skills: ['Накатываю Arch без гайда', 'трукодер'],
+    grafic: 'Удаленно 5/2',
+    pay: '50 000 руб',
+    published: '6 дней назад'
+  }
+];
--- src/scripts/resume-current.js
+++ src/scripts/resume-current.js
@@ -1,3 +1,8 @@
+// Пока что скрипты подключила по-старинке и css файл тоже отдельно, чтобы не мешать это все в основную ветку
+// Думаю можно скорректировать подключения, а также ссылку на новую страничку в самом конце перед сдачей
+
+// Конфигурационный объект
+
 const templateElementConfig = {
   resumeCurrentTemplateBlock: '#template',
   resumeCurrentCard: '.resume-current__card',
@@ -11,39 +16,11 @@ const templateElementConfig = {
   resumeCurrentImage: '.resume-current__image'
 }
 
-const resumeList = [
-  {
-    image: 'https://images.unsplash.com/photo-1610655507808-a59293f4e332?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8d29tYW4lMjBwb3J0cmFpdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
-    job: 'Психолог, координатор',
-    name: 'Иванова Мария Ивановна',
-    description: 'Творчески развиваюсь постоянно, изучаю новые методы и т.д. Ищу свою команду для активной деятельности во благо.',
-    skills: ['SMM', 'Instgram', 'Администратор', 'Открытость'],
-    grafic: 'Удаленно 5/2 2/2',
-    pay: '35 000 руб',
-    published: '1 день назад'
-  },
-  {
-    image: 'https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-    job: 'Преподватель, менеджер',
-    name: 'Смирнова Дарья Петровна',
-    description: 'Здравствуйте, меня зовут Дарья. Я занимаюсь преподаванием более 5 лет. Имею опыт работы с детьми от 4 и до 18 лет. Также занимаюсь со студентами и взрослыми. Преподавала в unknown школе. Нахожу ко всем индивидуальный подход.',
-    skills: ['Креативность', 'коммуникабельность'],
-    grafic: 'В офисе 5/2',
-    pay: '40 000 руб',
-    published: '4 дня назад'
-  },
-  {
-    image: 'https://images.unsplash.com/photo-1560298803-1d998f6b5249?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-    job: 'Прогаммист, системный администратор',
-    name: 'Иванов Иван Иванович',
-    description: 'Здравствуйте, меня зовут Иван. ',
-    skills: ['Накатываю Arch без гайда', 'трукодер'],
-    grafic: 'Удаленно 5/2',
-    pay: '50 000 руб',
-    published: '6 дней назад'
-  }
-];
+// Функция добавления карточки-резюме
 
+// Каждое резюме обернуто в ссылку на полный вариант с классом resume-current__link, нужно прописать ссылки в массиве и провести с ними все следующие шаги
+// То же касается и ховеров на каждой карточке при наведении. Ховер содержит название (пускай это будет специальность, у нас в объекте это элементы job) и описание (предлагаю там указать города к примеру)
+// как выглядят эти ховеры в идеале можно увидеть на нынешней страничке "Вакансии", я оставила их стили, должны сработать
 
 const templateElement = document.querySelector('#template').content;
 
@@ -76,10 +53,11 @@ function createCard(templateElementConfig, arrayElement) {
   resumeCurrentTemplateImage.alt = arrayElement.name;
 
   return resumeCurrentTemplate;
-}
+};
+
+// Создание карточки для каждого элемента массива
 
 resumeList.forEach((arrayElement) => {
   const resumeContainer = document.querySelector('.resume-current__main');
   resumeContainer.append(createCard(templateElementConfig, arrayElement));
 });
-
