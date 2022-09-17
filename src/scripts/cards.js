50480d64130766968cf827e9536f346bc5c1cc7e
Egor Talovikov <egtalovikov@gmail.com>
2022-09-17 10:23:30 +0300

    feat: добавлена страница 'Друзья Валеры' и lazy-loading

---
+++ src/scripts/cards.js
@@ -0,0 +1,62 @@
+const initialFriends = [
+  {
+    image: '../images/megafon.png',
+    name: 'Мегафон',
+    about: 'Предоставит услуги связи для некоммерческих организаций.'
+  },
+  {
+    image: '../images/lena-marshalova.png',
+    name: 'Лена Маршалова',
+    about: 'HR, профессиональный карьерный консультант и коуч.'
+  },
+  {
+    image: '../images/lena-marshalova.png',
+    name: 'Лена Маршалова',
+    about: 'HR, профессиональный карьерный консультант и коуч.'
+  },
+  {
+    image: '../images/lena-marshalova.png',
+    name: 'Лена Маршалова',
+    about: 'HR, профессиональный карьерный консультант и коуч.'
+  },
+  {
+    image: '../images/lena-marshalova.png',
+    name: 'Лена Маршалова',
+    about: 'HR, профессиональный карьерный консультант и коуч.'
+  },
+  {
+    image: '../images/lena-marshalova.png',
+    name: 'Лена Маршалова',
+    about: 'HR, профессиональный карьерный консультант и коуч.'
+  },
+  {
+    image: '../images/lena-marshalova.png',
+    name: 'Лена Маршалова',
+    about: 'HR, профессиональный карьерный консультант и коуч.'
+  },
+  {
+    image: '../images/lena-marshalova.png',
+    name: 'Лена Маршалова',
+    about: 'HR, профессиональный карьерный консультант и коуч.'
+  },
+  {
+    image: '../images/lena-marshalova.png',
+    name: 'Лена Маршалова',
+    about: 'HR, профессиональный карьерный консультант и коуч.'
+  },
+  {
+    image: '../images/lena-marshalova.png',
+    name: 'Лена Маршалова',
+    about: 'HR, профессиональный карьерный консультант и коуч.'
+  },
+  {
+    image: '../images/lena-marshalova.png',
+    name: 'Лена Маршалова',
+    about: 'HR, профессиональный карьерный консультант и коуч.'
+  },
+  {
+    image: '../images/lena-marshalova.png',
+    name: 'Лена Маршалова',
+    about: 'HR, профессиональный карьерный консультант и коуч.'
+  }
+];
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,9 +3,9 @@ const bannerButton = document.querySelector(".banner__button");
 if(bannerButton) {
   const bannerContainerContacts = document.querySelector(".banner__container-contancts");
   const bannerContainerContactsHiden = document.querySelector(".banner__container-contancts_hidden");
-  
-  bannerButton.addEventListener("click", () => {  
-      bannerContainerContacts.classList.toggle("banner__container-contancts_hidden");  
+
+  bannerButton.addEventListener("click", () => {
+      bannerContainerContacts.classList.toggle("banner__container-contancts_hidden");
       bannerButton.classList.toggle("banner__button_bc");
   })
 }
