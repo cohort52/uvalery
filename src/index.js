1bed281ae70f649ce6c49226c031c22ddce1343b
Tatiana Parfeniuk <tatiana.vik@gmail.com>
2022-09-18 16:41:22 +0300

    add: 3 imports to index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import "./scripts/script.js";
 import "./scripts/add-vacancy-validate.js";
+
+import "./scripts/similar-vacancy.js";
+import "./scripts/create-vacancy-cards.js";
+import "./scripts/carousel-slider.js";
