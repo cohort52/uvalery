f0888a8a3a0785d2be15f41ea49908a6c9fa9176
Tatiana Parfeniuk <tatiana.vik@gmail.com>
2022-09-18 18:33:08 +0300

    add: 3 js-files route to vacancy.html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import "./scripts/script.js";
 import "./scripts/add-vacancy-validate.js";
 
-import "./scripts/similar-vacancy.js";
-import "./scripts/create-vacancy-cards.js";
-import "./scripts/carousel-slider.js";
+
